e1f45444ffdc55387ed4f8e44de38c2303a7ef78
Me11ad <vlad505200483@gmail.com>
2023-11-15 09:53:17 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try{
+
+    await tcpClient.ConnectAsync("192.168.220.139",1777);
+    while(true)
+    {
+        System.Console.WriteLine("Введите команду для сервера");
+        string command = Console.ReadLine() + '\n';
+
+        byte[] requestData = Convert.ToByte;
+
+        await tcpClient.SendAsync(requestData);
+
+        Console.WriteLine("Сообщение отправлено");
+
+        byte[] data = new byte[512];
+
+        int bytes = await tcpClient.ReceiveAsync(data);
+
+        string time = Encoding.UTF8.GetString(data, 0, bytes);
+
+        Console.WriteLine($"Текущее время: (time)");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
