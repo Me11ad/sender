879715a649a164c0f9170ced77c605fbf120117b
Me11ad <vlad505200483@gmail.com>
2023-11-17 09:58:40 +0500

    fixed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Net.Sockets;
 
 using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 try{
@@ -8,18 +6,18 @@ try{
     await tcpClient.ConnectAsync("192.168.220.139",1777);
     while(true)
     {
-        System.Console.WriteLine("Введите команду для сервера");
+        byte[] imageData = File.ReadAllBytes("Изображения/3232332.jpg");
+
+        Console.WriteLine("Введите команду для сервера");
         string command = Console.ReadLine() + '\n';
 
-        byte[] imageData = File.ReadAllBytes("img/forVS.jpg");
-
-        byte[] requestData = File.ReadAllBytes("img/forVS.jpg");
+        byte[] requestData = File.ReadAllBytes("Изображения/3232332.jpg");
 
         await tcpClient.SendAsync(requestData, SocketFlags.None);
 
         Console.WriteLine("Сообщение отправлено");
 
-        byte[] data = new byte[512];
+        byte[] Data = new byte[512];
     }
 }
 catch (Exception ex)
